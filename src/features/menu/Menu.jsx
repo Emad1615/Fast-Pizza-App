58380d1368c859fe5547cdfd323998ce1ac19d39
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2024-02-29 10:33:16 +0200

    Scroll_GridView

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -10,25 +10,22 @@ function Menu() {
   const [isList,setIsList]=useState(true)
   return (
     <div>
-      <p className="bg-yellow-100 p-2 text-center font-semibold uppercase text-xl">
-        Offer buy <strong className="text-red-500">1</strong> get{" "}
-        <strong className="text-red-500">3</strong>
-      </p>
+      <marquee  direction="down" scrolldelay="350" behavior="alternate" className="bg-slate-50 p-2 text-center font-bold uppercase text-xl tracking-widest leading-10">Your offer belong here </marquee>
       <div className=" py-2 flex justify-center items-center gap-3 ">
-          <button onClick={()=>setIsList(true)} className="text-2xl focus:outline-none bg-slate-200 p-2 rounded" title="List view"><CiBoxList /></button>
-          <button onClick={()=>setIsList(false)} className="text-2xl focus:outline-none bg-slate-200 p-2 rounded" title="Grid view"><CiGrid41 /></button>
+          <button onClick={()=>setIsList(true)} className="text-2xl focus:outline-none bg-stone-200 p-2 rounded  font-semibold hover:bg-stone-300 transition-colors duration-300 ease-in" title="List view"><CiBoxList /></button>
+          <button onClick={()=>setIsList(false)} className="text-2xl focus:outline-none bg-stone-200 p-2 rounded  font-semibold hover:bg-stone-300 transition-colors duration-300 ease-in" title="Grid view"><CiGrid41 /></button>
       </div>
       {
-        isList?<ul className="px-5 py-2 divide-y divide-slate-200">
+        isList? <ul className="px-5 py-2 divide-y divide-slate-200 ">
         {pizzas.map((pizza, idx) => (
-          <MenuItem pizza={pizza} key={idx} />
+          <MenuItem pizza={pizza} key={idx} isList={isList}/>
         ))}
-        </ul>:
-        <ul className="grid  xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-1  gap-6 p-5  ">
-          {pizzas.map((pizza, idx) => (
-            <MenuItem pizza={pizza} key={idx} isList={isList}/>
-          ))}
-        </ul>
+          </ul> :
+          <ul className="grid  xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-1  gap-6 p-5  ">
+            {pizzas.map((pizza, idx) => (
+              <MenuItem pizza={pizza} key={idx} isList={isList}/>
+            ))}
+          </ul>
       }
     </div>
   );
